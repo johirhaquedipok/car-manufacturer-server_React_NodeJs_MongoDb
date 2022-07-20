941e67e5201b4938c8615f5a50906f229d226079
unknown <johirhaquedipok2@gmail.com>
2022-07-20 08:26:14 +0600

    mongo db connect

--- index.js
+++ index.js
@@ -5,6 +5,71 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
+// mongodb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// verifyToken
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// mongo db connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.amtbqis.mongodb.net/?retryWrites=true&w=majority`;
+
+// create client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const productsCollection = client
+      .db("sonikon_global")
+      .collection("allProducts");
+    const orderedCollection = client
+      .db("sonikon_global")
+      .collection("orderedProducts");
+    const userCollection = client.db("sonikon_global").collection("users");
+    const adminCollection = client.db("sonikon_global").collection("admin");
+
+    /*
+     * get : All get collection
+     */
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    console.log("connected");
+  } finally {
+  }
+}
+
+run().catch(console.dir());
+
 app.get("/", async (req, res) => {
   res.send("Hello World");
 });
